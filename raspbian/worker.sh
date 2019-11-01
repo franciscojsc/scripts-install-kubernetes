ff1a7e3e54fbce6ccc0ef72543962e0bd91ec43b
Francisco Rodrigues <frchico@gmail.com>
2019-11-01 20:18:56 -0300

    Added command to join into cluster

--- raspbian/worker.sh
+++ raspbian/worker.sh
@@ -21,10 +21,6 @@ echo "       ██╔═██╗ ██╔══██╗╚════██
 echo "       ██║  ██╗╚█████╔╝███████║                              ";
 echo "       ╚═╝  ╚═╝ ╚════╝ ╚══════╝                              ";
 echo "                                                             ";
-
-
-echo                                                     
-
 echo  "__        __         _               _   _           _       "
 echo  "\ \      / /__  _ __| | _____ _ __  | \ | | ___   __| | ___  "
 echo  " \ \ /\ / / _ \| '__| |/ / _ \ '__| |  \| |/ _ \ / _\` |/ _ \ "
@@ -32,7 +28,7 @@ echo  "  \ V  V / (_) | |  |   <  __/ |    | |\  | (_) | (_| |  __/ "
 echo  "   \_/\_/ \___/|_|  |_|\_\___|_|    |_| \_|\___/ \__,_|\___| "
 
 
-sleep 5
+read -p " $(date +%H:%M:%S) Press CRTL+C to abort... " -t 5
 
 echo  
 echo "**** Config raspberry pi 3 to work node master with k8s, Docker and Helm *****"
@@ -49,8 +45,6 @@ echo "source <(kubectl completion bash)" >> $HOME/.bashrc
 
 
 
-
-
 echo "███████╗██╗███╗   ██╗██╗███████╗██╗  ██╗    ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ";
 echo "██╔════╝██║████╗  ██║██║██╔════╝██║  ██║    ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ";
 echo "█████╗  ██║██╔██╗ ██║██║███████╗███████║    ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     ";
@@ -58,3 +52,25 @@ echo "██╔══╝  ██║██║╚██╗██║██║╚═
 echo "██║     ██║██║ ╚████║██║███████║██║  ██║    ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗";
 echo "╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝    ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝";
 echo "                                                                                                  ";
+
+echo "Do you know the parameters (ip:port, token and cert-hash) for joining the cluster?[yes/no]: "
+read answer
+if [ -z $answer -o $answer != 'yes' ]
+then
+	echo  
+	echo "**** The next step is to join with the master node.                     *****"
+	echo "**** Execute [kubeadm token create --print-join-command] to get the key *****"
+	echo "**** and then join the cluster                                          *****"
+	echo 
+else
+    echo "Server IP:"
+	read IP
+	echo "Token:"
+	read token
+	echo "Hash:"
+	read hash
+
+kubeadm join $IP --token $token \
+    	--discovery-token-ca-cert-hash $hash
+
+fi
